7a992ff079d51981e2d80e4b1add270c442d373f
Luis Fernando Hernandez <hluisfernando1@gmail.com>
2021-04-11 23:11:36 -0500

    two components are created that communicate through the parent component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Navbarm } from './components/Navbarm'
 import { Home } from './components/Home'
 
+class App extends React.Component {
+  
+  constructor() {
+    super()
+    this.childHome = React.createRef()
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Navbarm/>
-      <Home/>
-    </div>
-  );
+
+  handleSearch = (e) => {
+    this.childHome.current.getIechoApi(e.target.value)
+  }
+
+  
+  render() {
+    return (
+      <div className="App">
+        <Navbarm handleSearch={this.handleSearch}/>
+        <Home ref={this.childHome}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Navbarm.js
+++ src/components/Navbarm.js
@@ -1,12 +1,35 @@
-import React, {Component} from 'react';
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
 import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
 import logo from '../assets/img/logo.png';
 
 
 
-export class Navbarm extends Component {
+
+export class Navbarm extends React.Component  {
+
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+
+    handleChange(event) {
+        let regex = new RegExp("^[a-zA-Z ]+$");
+        if (regex.test(event.target.value)) {
+            this.setState({value: event.target.value});
+        } else {
+            if(event.target.value.length === 0){
+                this.setState({value: ''});
+            }
+            return false
+        }
+    } 
     
+  
     render(){
+        const { handleSearch } = this.props
         return(
             <Navbar className="navbar-style" variant="light" collapseOnSelect expand="lg">
                 <Navbar.Brand>
@@ -17,8 +40,8 @@ export class Navbarm extends Component {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse className="justify-content-center" id="responsive-navbar-nav">
                 <Form inline>
-                    <FormControl type="text" placeholder="Insert Text" className="mr-sm-2" />
-                    <Button variant="outline-light">Send</Button>
+                    <FormControl value={this.state.value} onChange={this.handleChange} type="text" placeholder="Insert Text" className="mr-sm-2" />
+                    <Button onClick={handleSearch} value={this.state.value} variant="outline-light">Send</Button>
                 </Form>
                 </Navbar.Collapse>
             </Navbar>
